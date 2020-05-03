45d97767f51a7a8bc24bf1a885d6f9486ad95585
7Aishwarya <7aishwaryasharma@gmail.com>
2020-05-03 20:35:00 +0530

    react

---
+++ React/mapping-components-practice/src/emojipedia.js
@@ -0,0 +1,60 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🤪",
+    name: " Crazy Face",
+    meaning:
+      "Something is insanely funny. The mood is exuberant - you are totally silly and crazy. Totally freaking out with enthusiasm or joy. Maybe someone has even told an indecent joke."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😁",
+    name: "Grinning Face With Smiling Eyes",
+    meaning:
+      "Happy face with a mischievous laugh. Represents lightheartedness and exuberance. Full of joy due to an event, excited and agitated or just a bit embarrassed."
+  },
+  {
+    id: 5,
+    emoji: "🥳",
+    name: "Face with party blower and party hat",
+    meaning:
+      "That was a wild party or “Let's celebrate“. No matter what joyful event is in view, it will be celebrated appropriately. May also stand for the carnival season."
+  },
+  {
+    id: 6,
+    emoji: "🤩",
+    name: "Smirking Face With Starry Eyes",
+    meaning:
+      "Is very excited about something and already full of anticipation. Overwhelmed and speechless after meeting someone you like a lot. Fascinated by the glory and the world of the stars."
+  },
+  {
+    id: 7,
+    emoji: "😊",
+    name: "Smiling Face With Smiling Eyes",
+    meaning:
+      "Is grinning shyly and embarrassed. The red cheeks are an expression of joy. Represents happiness, contentment, peace of mind and gratitude. Text-based variant: ^^."
+  },
+  {
+    id: 8,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "Smiley blinks mischievously with one eye. Expression of humor, has been pulling a practical joke, is flirting with you or has an ulterior motive. The wink weakens the message: do not take it seriously, regard it as humorous."
+  }
+];
+
+export default emojipedia;
