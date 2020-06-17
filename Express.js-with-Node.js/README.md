1b69ad6351626b00a6ad569b1b51056441486d22
7Aishwarya <7aishwaryasharma@gmail.com>
2020-06-17 22:07:26 +0530

    my first express server-calculating sum

---
+++ Express.js-with-Node.js/README.md
@@ -0,0 +1,10 @@
+Set up a new npm package
+**npm init**
+
+Intall packages:
+**npm install express**
+**npm install -g nodeman**
+**npm install body-parser**
+
+
+
---
+++ Express.js-with-Node.js/index.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser=require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    var num1 = Number(req.body.n1);
+    var num2 = Number(req.body.n2);
+    var result = num1 + num2;
+    res.send("The result of calculation is " + result);
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port = 3000");
+});
